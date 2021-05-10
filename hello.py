587503c86e2316e0141d305331ac7c61d1aa6e1f
guowangbixia <2231400200@qq.com>
2021-05-10 09:30:14 +0800

    Initial commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a=12+11
+    tup1 = ('physics', 'chemistry', 1997, 2000)
+    tup2 = (1, 2, 3, 4, 5)
+    list1 = ["a", "b", "c", "d","e"]
+    list2=['e','f']
+    set1={1,2,3}
+    set2={1,3,2}
+    list3=list1+list2
+    tup4=tup1+tup2
+    x='asdf'
+    print(set2*2)
